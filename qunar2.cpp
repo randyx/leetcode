58d003e89435c6c8786ce09b01f0d291cc2e113f
randyx <xiao93qi@gmail.com>
2015-03-18 13:06:02 +0800

    qunar2

---
+++ qunar2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+char zifu(string target);
+int main(){
+/*	char a[10]="Hello";
+	char* str="Hello";
+	cout<<*str<<endl;
+	cout<<str<<endl;
+	cout<<a<<endl;*/
+	string target1;//vc 6.0bug   target1.at(1)
+	getline(cin,target1);
+	//cout<<target1<<endl<<target1.size()<<endl<<target1.at(1)<<endl<<target1[1]<<endl;
+	cout<<zifu(target1)<<endl;
+}
+char zifu(string target){
+	string::size_type position;
+	int m=0;
+	while(m<target.size()){
+	position=target.find(target[m],m+1);
+	if (position!=string::npos) return target[m];
+	m=m+1;
+	}
+}
+

3987b2ce5356f07ce64ea95def9684633322c089
randyx <xiao93qi@gmail.com>
2016-05-02 23:44:13 +0800

    Revert "136 c"
    This reverts commit 37faf1fb2b7b59d447bf20544e2d1f137b2f453f.

--- 136.c
+++
@@ -1,8 +0,0 @@
-int singleNumber(int* nums, int numsSize) {
-    if(nums == NULL)
-        return 0;
-    int result = nums[0];
-    for(int i =1 ; i < numsSize; i++)
-        result = result ^ nums[i];
-    return result;
-}
